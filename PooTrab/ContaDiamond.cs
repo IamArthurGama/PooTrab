aa3445eb73d1da3c7877b282c3cb02bc8f03a077
ThalesCopola <thalespaivacopola@gmail.com>
2025-11-04 17:41:14 -0300

    Classe conta gold e diamond

---
+++ PooTrab/ContaDiamond.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTrab
+{
+    public class ContaDiamond : Conta
+    {
+        private decimal _tarifa;
+
+        public ContaDiamond(Cliente cliente, Banco banco, string codigoConta, decimal saldoInicial, decimal tarifa)
+            : base(cliente, banco, codigoConta, saldoInicial)
+        {
+            _tarifa = tarifa;
+        }
+
+        public ContaDiamond(Cliente cliente, Banco banco, string codigoConta, decimal tarifa)
+            : base(cliente, banco, codigoConta)
+        {
+            _tarifa = tarifa;
+        }
+        public decimal Tarifa
+        {
+            get => _tarifa;
+            set => _tarifa = value;
+        }
+
+        //implementar os metodos ainda
+    }
+}
---
+++ PooTrab/ContaGold.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTrab
+{
+    public class ContaGold : Conta
+    { 
+        private decimal _taxaSaque;
+        private decimal _taxaTransferencia;
+        private decimal _tarifa;
+
+        public ContaGold(Cliente cliente, Banco banco, string codigoConta, decimal saldoInicial,
+              decimal taxaSaque, decimal taxaTransferencia, decimal tarifa)
+            : base(cliente, banco, codigoConta, saldoInicial)
+        {
+            _taxaSaque = taxaSaque;
+            _taxaTransferencia = taxaTransferencia;
+            _tarifa = tarifa;
+        }
+
+        
+        public ContaGold(Cliente cliente, Banco banco, string codigoConta,
+              decimal taxaSaque, decimal taxaTransferencia, decimal tarifa)
+            : base(cliente, banco, codigoConta)
+        {
+            _taxaSaque = taxaSaque;
+            _taxaTransferencia = taxaTransferencia;
+            _tarifa = tarifa;
+        }
+
+       
+        public decimal TaxaSaque
+        {
+            get => _taxaSaque;
+            set => _taxaSaque = value;
+        }
+
+        public decimal TaxaTransferencia
+        {
+            get => _taxaTransferencia;
+            set => _taxaTransferencia = value;
+        }
+
+        public decimal Tarifa
+        {
+            get => _tarifa;
+            set => _tarifa = value;
+        }
+
+        //implementar os metodos ainda
+
+    }
+}
