08ba8069eacc9f576ad7e79c18b6d08fc409153d
Arthur Gama <114451612+IamArthurGama@users.noreply.github.com>
2025-11-03 21:52:03 -0300

    conta e cliente

--- PooTrab/Conta.cs
+++ PooTrab/Conta.cs
@@ -8,5 +8,20 @@ namespace PooTrab
 {
     abstract class Conta
     {
+
+        //        - cliente: Cliente
+        //- banco: Bancol
+        //- codigoConta: string
+        //- saldo: decimal
+        private Cliente _cliente;
+        private Banco _banco;
+        private string _codigoConta;
+        private decimal _saldo;
+
+
+        public Conta(Cliente cliente, )
+        {
+
+        }
     }
 }
