a4cd0b5aa2756c7a71e0733437d4727fbefd3247
Erick Ribeiro <ericktho202@gmail.com>
2025-11-03 21:46:22 -0300

    teste

---
+++ PooTrab/Banco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTrab
+{
+    public class Banco
+    {
+        //Atributos
+        private string _nome;
+        private string _codigoBanco;
+        private string _cnpj;
+
+        //Prop de leitura
+        public string Nome
+        {
+            get { return _nome; }
+        }
+        public string CodigoBanco => _codigoBanco;// Prop de leitura simplificada com arrow "=>" ambas significam que apenas o get é publico
+        public string Cnpj => _cnpj;
+
+        //Construtor
+        public Banco(string nome, string codigoBanco, string cnpj)
+        {
+            _nome = nome;
+            _codigoBanco = codigoBanco;
+            _cnpj = cnpj;
+        }
+    }
+}
