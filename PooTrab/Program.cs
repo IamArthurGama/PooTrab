f999bd1989011a7a8c901b1927b79cf9c0626095
Arthur Gama <114451612+IamArthurGama@users.noreply.github.com>
2025-11-10 20:51:29 -0300

    Classe Terminal

--- PooTrab/Program.cs
+++ PooTrab/Program.cs
@@ -1,6 +1,6 @@
 ﻿using PooTrab;
 
-Console.WriteLine("Inicio do exemplo de abstração!\n\n");
+Terminal.Narrador("Inicio do exemplo de abstração!\n\n");
 
 Banco banco1 = new ("Nubank", "123", "12345678912345");
 Console.WriteLine(
@@ -17,6 +17,29 @@ Console.WriteLine(
     $"Telefone: {cliente1.Telefone}\n" +
     $"Endereço: {cliente1.Endereco}\n\n");
 
+Terminal.Narrador(
+    $"Banco e Cliente são requisitos para o nosso exemplo de abstração acontecer.\n\n" +
+    $"Conta será nossa classe abstrata, nela que teremos um molde de onde todas as outras derivadas vão se basear.\n\n"
+);
+
+Terminal.Narrador(
+    $"Conta sempre pede como padrão os seguintes parâmetros:\n" +
+    $"- Cliente\n" +
+    $"- Banco\n" +
+    $"- Codigo da Conta\n" +
+    $"- Saldo (Sendo Opcional)\n" +
+    $"(Cada parametro possui suas propriedades.)\n\n" +
+    $"Possue os seguintes métodos:\n" +
+    $"- Sacar()\n" +
+    $"- Transferir()\n" +
+    $"- Depositar()\n" +
+    $"- VerificarSaldo()\n\n"
+);
+
+Terminal.Narrador($"A primeira Conta a partir da classe abstrata será ContaFree, suas particularidades são suas taxas de saque e de transferencias.");
+
+ContaFree conta1 = new(cliente1, banco1, "2134");
+Console.WriteLine();
 
 
 
---
+++ PooTrab/Terminal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTrab
+{
+    public static class Terminal
+    {
+        public static void Narrador(string Text)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine(Text);
+            
+            Console.ResetColor();
+        }
+    }
+}
